48c292d024f123166556379b6d3a3ba3d510d397
justiceadamsUNI <adamsjal@uni.edu>
2016-10-22 20:07:54 -0500

    Unit test done. Happy day!

--- Convex Hull Problem/convex_hull.py
+++ Convex Hull Problem/convex_hull.py
@@ -5,15 +5,13 @@
 ## hull that would contain all the points given. I decided to leave out a
 ## user input feature, and instead you must directly alter the array at the
 ## bottom of this file in order to change the input. The focus shouldn't be
-## on UX, but rather the implementation... so I ommited it. Please keep
+## on UX, but rather the implementation... so I omitted it. Please keep
 ## floats under 10 digits to avoid floating point errors with specific
 ## geometric calculations.
 
-## Note: this algoritm is set up such that if given a set of points that are
-## all colinear, it will just return all points. Therefore it will let the line
-## itslef be the convex hull.
-
-print = print
+## Note: this algorithm is set up such that if given a set of points that are
+## all collinear, it will just return all points. Therefore it will let the line
+## itself be the convex hull.
 
 MATPLOTLIB_INSTALLED = True
 try:
@@ -21,53 +19,57 @@ try:
 except ImportError:
     MATPLOTLIB_INSTALLED = False
 
+print = print  ##This is necessary for mocking the print function in unit test.
+input = input  ##This is necessary for mocking the input function in unit test.
 X_VAL = 0
 Y_VAL = 1
 
+
 def getPointsOfConvexHull(points_in_plane):
     """Takes in an array of tuples that represent a set of points in the XY plane, and returns an Array containing all points that make up
-       the convex hull."""
-    
+       the convex hull. Also prepares necessary lines for plotting if MatPlotLib installed on machine."""
+
     points_of_convex_hull = []
     n = len(points_in_plane)
-    
-    for i in range(0,n-1):
+
+    for i in range(0, n - 1):
         pointI = points_in_plane[i]
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             pointJ = points_in_plane[j]
-            
+
             if lineIsBorderOfHull(pointI, pointJ, points_in_plane):
-                if MATPLOTLIB_INSTALLED:
-                    prepareLineForPlot(pointI, pointJ)
                 if pointI not in points_of_convex_hull:
                     points_of_convex_hull.append(pointI)
                 if pointJ not in points_of_convex_hull:
                     points_of_convex_hull.append(pointJ)
-                        
+
+                if MATPLOTLIB_INSTALLED:
+                    prepareLineForPlot(pointI, pointJ)
+
     return points_of_convex_hull
 
 
 def prepareLineForPlot(point1, point2):
-    """Takes in two points on the XY plane and uses the external library MatPlotLib to graph a green line between the two points"""
-    
+    """Takes in two points on the XY plane and uses the external library MatPlotLib to graph a green line between the two points.
+        Graphic won't appear until show() is called."""
+
     plt.plot((point1[X_VAL], point2[X_VAL]), (point1[Y_VAL], point2[Y_VAL]), 'g-', lw=2)
 
-    
+
 def lineIsBorderOfHull(point1, point2, points_in_plane):
     """Takes in 2 points (tuples), constructs a line between them, and determines if the given line makes up one of the borders of the
        convex hull. If every other point lies on that line, this will return true."""
-    
+
     line_equation = constructLineBetweenPoints(point1, point2)
     first_points_position_relative_to_line = None
     n = len(points_in_plane)
 
-    for i in range(0,n):
+    for i in range(0, n):
         point = points_in_plane[i]
-
         if getPointsPositionRelativeToLine(point, line_equation) == 0:
             continue
         else:
-            if first_points_position_relative_to_line == None:
+            if first_points_position_relative_to_line is None:
                 first_points_position_relative_to_line = getPointsPositionRelativeToLine(point, line_equation)
                 continue
 
@@ -77,10 +79,10 @@ def lineIsBorderOfHull(point1, point2, points_in_plane):
     return True
 
 
-def constructLineBetweenPoints(point1,point2):
-    """Takes in 2 points (tuples) and returns a two variable function representing the line between those two points. Recognizes vertical
+def constructLineBetweenPoints(point1, point2):
+    """Takes in 2 points (tuples) and returns a two variable lambda representing the line between those two points. Recognizes vertical
        lines and returns according function. Each equation set = 0 represents all points on said line."""
-    
+
     point1_x = point1[X_VAL]
     point1_y = point1[Y_VAL]
 
@@ -88,18 +90,17 @@ def constructLineBetweenPoints(point1,point2):
     point2_y = point2[Y_VAL]
 
     delta_x = (point2_x - point1_x)
-
     if delta_x == 0:
         return lambda x, y: x - point2_x
     else:
-        slope = (point2_y - point1_y)/(point2_x - point1_x)
-        return lambda x, y: slope*(x - point1_x) + point1_y - y
+        slope = (point2_y - point1_y) / (point2_x - point1_x)
+        return lambda x, y: slope * (x - point1_x) + point1_y - y
 
 
 def getPointsPositionRelativeToLine(point, line_equation):
-    """Takes in a 2 variable function representing the equation of a line and any point on the plane (Tuple), then determines that points
+    """Takes in a 2 variable lambda representing the equation of a line and any point on the plane (Tuple), then determines that points
        location in relation to the given line. To avoid floating point error we round each result to 9 digits. See preface
-       instructions."""
+       instructions. Returns 0 for a collinear point."""
 
     equation_at_point = round(line_equation(point[X_VAL], point[Y_VAL]), 10)
 
@@ -112,7 +113,7 @@ def getPointsPositionRelativeToLine(point, line_equation):
 
 
 def printPointsOfConvexHull(points_of_convex_hull):
-    """Displays results by printing each point (ordered pair) making up the convex hull."""
+    """Displays results by printing each point (tuple) making up the convex hull."""
 
     print("Script Over. Below are the following points that make up the convex hull:")
 
@@ -122,44 +123,50 @@ def printPointsOfConvexHull(points_of_convex_hull):
 
 def promptForValidBoolean(message):
     """Prompts the user to enter a valid boolean (y) or (n) for a given message. Will not accept a faulty answer."""
-    
-    while(True):
+
+    while True:
         user_response = input("\n" + message + " (y)es or (n)o: ")
         if user_response.lower().strip() in ("y", "n"):
             user_response = (user_response == "y")
             break
         else:
-            print("Invlid input. Enter (y)es or (n)o")
+            print("Invalid input. Enter (y)es or (n)o")
 
     return user_response
 
 
-def displayConvexHull(all_points_in_plane):
+def displayConvexHull(points_in_plane):
     """Displays convex hull scatter plot using external MatPlotLib library."""
-    
+
     x_vals = []
     y_vals = []
-    
-    for point in (all_points_in_plane):
+
+    for point in points_in_plane:
         x_vals.append(point[X_VAL])
         y_vals.append(point[Y_VAL])
 
-    plt.scatter(x_vals,y_vals)
+    plt.scatter(x_vals, y_vals)
     plt.show()
 
+
 def main():
-    """Runs algorithm for the given set of points defined below. Prints the output and gives option to display graphic of convex hull."""
+    """Runs algorithm for the given set of points defined below. Prints the output and gives option to display graphic of convex hull.
+        if MatPlotLib installed."""
 
     ##Data set of points in plane. Change Accordingly.
-    points_in_plane = [(-2,5), (1,1), (2,15), (30,30), (10,10), (15, 15), (30,1), (20,3), (25,10), (29, 2)]
-    show_graphic = False
-    
+    points_in_plane = [(-2, 5), (1, -5), (2, 15), (30, 30), (10, 10), (15, 15), (30, -10), (20, 3), (25, 10), (29, 2),
+                       (-10, 10), (19, 19), (10, 9.8), (50, 45), (20, 67)]
     points_of_convex_hull = getPointsOfConvexHull(points_in_plane)
     printPointsOfConvexHull(points_of_convex_hull)
+
+    show_graphic = False
     if MATPLOTLIB_INSTALLED:
         show_graphic = promptForValidBoolean("Would you like to see a graphic of the convex hull?")
+
     if show_graphic:
         displayConvexHull(points_in_plane)
-    
+
+
 if __name__ == '__main__':
+    ##This keeps the script from running upon importing. Has to be explicitly excecuted.
     main()
--- Convex Hull Problem/test_convex_hull.py
+++ Convex Hull Problem/test_convex_hull.py
@@ -1,149 +1,183 @@
+## Author:   Justice Adams
+## Unit test for convex_hull script
+
+## You must have installed the libraries mock
+## (https://docs.python.org/dev/library/unittest.mock.html) as well
+## as MatPlotLib (http://matplotlib.org/users/installing.html) for this
+## test to be fully functional/exhaustive.
+
 import unittest
 from unittest import mock
 import random
 from mock import patch
-import convex_hull as convex_hull
+import convex_hull
 
 
 class TestConvexHull(unittest.TestCase):
+    mSquareInPlane = [(0, 0), (0, 2), (2, 0), (2, 2)]
+    mCollinearPointsInPlane = [(0, 0), (10, 10), (1, 1), (-1, -1)]
+    mYEqualsX = staticmethod(lambda x, y: (x - 1) + 1 - y)
+    X_VAL = 0
+    Y_VAL = 1
 
     def setUp(self):
         print("Testing: ", self._testMethodName)
 
 
-    @patch('matplotlib.pyplot.plot')
-    def test_prepareLineForPlot(self, mock):
-        convex_hull.prepareLineForPlot((0,0), (1,1))
-        self.assertTrue(mock.called)
-
-
-    def test_lineIsBorderOfHullWithValidBorder(self):
-        corner1 = (0,0)
-        corner2 = (0,2)
-        corner3 = (2,2)
-        corner4 = (2,0)
-        square_in_plane = [corner1, corner2, corner3, corner4]
-
-        self.assertTrue(convex_hull.lineIsBorderOfHull(corner1, corner2, square_in_plane))
-
-
-    def test_lineIsBorderOfHullWithInValidBorder(self):
-        corner1 = (0,0)
-        corner2 = (0,2)
-        corner3 = (2,2)
-        corner4 = (2,0)
-        square_in_plane = [corner1, corner2, corner3, corner4]
-
-        self.assertFalse(convex_hull.lineIsBorderOfHull(corner1, corner3, square_in_plane))
-
-
-    def test_lineIsBorderOfHullWithColinearPoints(self):
-        vector_tail = (0,0)
-        vector_head = (10,10)
-        point_1_on_line = (1,1)
-        point_2_on_line = (-1,1)
-        
-        colinear_points = [vector_tail, vector_head, point_1_on_line, point_2_on_line]
-
-        self.assertTrue(convex_hull.lineIsBorderOfHull(vector_tail, vector_head, colinear_points))
-
-
     def test_constructLineBetweenPointsWithNonVerticalLine(self):
-        point1 = (1,1)
-        point2 = (5,5)
+        point1 = (1, 1)
+        point2 = (5, 5)
         line = convex_hull.constructLineBetweenPoints(point1, point2)
-        y_equals_x = lambda x, y: (x - point1[0]) + point1[1] - y
 
-        test_input = generateTestInput()
-
-        for test_point in test_input:
-           test_x = test_point[0]
-           test_y = test_point[1]
-            
-           self.assertEqual(line(test_x, test_y), y_equals_x(test_x, test_y))
+        assertLineEquationLambdasEqual(self, line, self.mYEqualsX)
 
 
     def test_constructLineBetweenPointsWithVerticalLine(self):
-        point1 = (1,1)
-        point2 = (1,5)
+        point1 = (1, 1)
+        point2 = (1, 5)
         line = convex_hull.constructLineBetweenPoints(point1, point2)
-        vertical_line = lambda x, y: x - point2[0]
+        x_equals_1 = lambda x, y: x - 1
 
-        test_input = generateTestInput()
-
-        for test_point in test_input:
-           test_x = test_point[0]
-           test_y = test_point[1]
-            
-           self.assertEqual(line(test_x, test_y), vertical_line(test_x, test_y))
-
-
-    def test_getPointsPositionRelativeToLineWithHigherPoint(self):
-        y_equals_x = lambda x, y: (x - 1) + 1 - y
-        point = (1,10)
-
-        points_position_relative_to_line = convex_hull.getPointsPositionRelativeToLine(point, y_equals_x)
-        self.assertEqual(-1, points_position_relative_to_line)
-
-
-    def test_getPointsPositionRelativeToLineWithLowerPoint(self):
-        y_equals_x = lambda x, y: (x - 1) + 1 - y
-        point = (10,1)
-
-        points_position_relative_to_line = convex_hull.getPointsPositionRelativeToLine(point, y_equals_x)
-        self.assertEqual(1, points_position_relative_to_line)
-
-
-    def test_getPointsPositionRelativeToLineWithColinearPoint(self):
-        y_equals_x = lambda x, y: (x - 1) + 1 - y
-        point = (10,10)
-
-        points_position_relative_to_line = convex_hull.getPointsPositionRelativeToLine(point, y_equals_x)
-        self.assertEqual(0, points_position_relative_to_line)
+        assertLineEquationLambdasEqual(self, line, x_equals_1)
 
 
     @patch('matplotlib.pyplot.show')
     @patch('matplotlib.pyplot.scatter')
     def test_displayConvexHull(self, mock_scatter, mock_show):
-        test_input = generateTestInput()
+        test_input = generatePointsInPlane()
 
         x_vals = []
         y_vals = []
         for point in test_input:
-            x_vals.append(point[0])
-            y_vals.append(point[1])
+            x_vals.append(point[self.X_VAL])
+            y_vals.append(point[self.Y_VAL])
 
         convex_hull.displayConvexHull(test_input)
         mock_scatter.assert_called_once_with(x_vals, y_vals)
         self.assertTrue(mock_show.called)
-        
+
+
+    def test_getPointsOfConvexHull(self):
+        input_data = [self.mSquareInPlane[0], self.mSquareInPlane[1], self.mSquareInPlane[2], self.mSquareInPlane[3],
+                      (1, 1), (1, 1.75)]
+
+        assertListsEqual(self, self.mSquareInPlane, convex_hull.getPointsOfConvexHull(input_data))
+
+
+    def test_getPointsOfConvexHullWithCollinearPoints(self):
+        assertListsEqual(self, self.mCollinearPointsInPlane,
+                         convex_hull.getPointsOfConvexHull(self.mCollinearPointsInPlane))
+
+
+    def test_getPointsPositionRelativeToLineWithCollinearPoint(self):
+        point = (10, 10)
+
+        points_position_relative_to_line = convex_hull.getPointsPositionRelativeToLine(point, self.mYEqualsX)
+        self.assertEqual(0, points_position_relative_to_line)
+
+
+    def test_getPointsPositionRelativeToLineWithHigherPoint(self):
+        point = (1, 10)
+
+        points_position_relative_to_line = convex_hull.getPointsPositionRelativeToLine(point, self.mYEqualsX)
+        self.assertEqual(-1, points_position_relative_to_line)
+
+
+    def test_getPointsPositionRelativeToLineWithLowerPoint(self):
+        point = (10, 1)
+
+        points_position_relative_to_line = convex_hull.getPointsPositionRelativeToLine(point, self.mYEqualsX)
+        self.assertEqual(1, points_position_relative_to_line)
+
+
+    def test_lineIsBorderOfHullWithCollinearPoints(self):
+        vector_tail = self.mCollinearPointsInPlane[0]
+        vector_head = self.mCollinearPointsInPlane[1]
+
+        self.assertTrue(convex_hull.lineIsBorderOfHull(vector_tail, vector_head, self.mCollinearPointsInPlane))
+
+
+    def test_lineIsBorderOfHullWithInValidBorder(self):
+        lower_left_corner = self.mSquareInPlane[0]
+        upper_right_corner = self.mSquareInPlane[3]
+
+        self.assertFalse(convex_hull.lineIsBorderOfHull(lower_left_corner, upper_right_corner, self.mSquareInPlane))
+
+
+    def test_lineIsBorderOfHullWithValidBorder(self):
+        lower_left_corner = self.mSquareInPlane[0]
+        upper_left_corner = self.mSquareInPlane[1]
+
+        self.assertTrue(convex_hull.lineIsBorderOfHull(lower_left_corner, upper_left_corner, self.mSquareInPlane))
+
 
     @patch("convex_hull.print", autospec=True)
-    def test_printPointsOfConvexHull(self,  mock_print):
-        test_input = generateTestInput()
+    @patch("convex_hull.input", return_value="n")
+    @patch('convex_hull.printPointsOfConvexHull')
+    @patch('convex_hull.getPointsOfConvexHull')
+    def test_main(self, mock_getPointsOfConvexHull, mock_printPointsOfConvexHull, mock_input, mock_print):
+        convex_hull.main()
+
+        self.assertTrue(mock_getPointsOfConvexHull.called)
+        self.assertTrue(mock_printPointsOfConvexHull.called)
+
+
+    @patch('matplotlib.pyplot.plot')
+    def test_prepareLineForPlot(self, mock_plot):
+        convex_hull.prepareLineForPlot((0, 0), (1, 1))
+        mock_plot.assert_called_with((0, 1), (0, 1), 'g-', lw=2)
+
+
+    @patch("convex_hull.print", autospec=True)
+    def test_printPointsOfConvexHull(self, mock_print):
+        test_input = generatePointsInPlane()
         convex_hull.printPointsOfConvexHull(test_input)
 
         calls = []
         for point in test_input:
             calls.append(mock.call("\t" + str(point)))
 
-        mock_print.assert_has_calls(calls, any_order=True)
-        
-        
+        mock_print.assert_has_calls(calls, any_order=False)
+
+
+    @patch("convex_hull.input", return_value="n")
+    def test_promptForValidBooleanWithInputValueN(self, mock_input):
+        result = convex_hull.promptForValidBoolean("test message")
+
+        self.assertFalse(result)
+
+
+    @patch("convex_hull.input", return_value="y")
+    def test_promptForValidBooleanWithInputValueY(self, mock_input):
+        result = convex_hull.promptForValidBoolean("test message")
+
+        self.assertTrue(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
 
 ## HELPER METHODS
-def generateTestInput(): ##Change name of method
+def generatePointsInPlane():
     test_input = []
     for i in range(10):
         x = random.randrange(-100, 100)
         y = random.randrange(-100, 100)
-        test_input.append((x,y))
+        test_input.append((x, y))
 
     return test_input
 
-def testPrintStatement(string):
-    print(string)
-        
 
-if __name__ == "__main__":
-    unittest.main()
+def assertLineEquationLambdasEqual(context, line_equation_1, line_equation_2):
+    test_input = generatePointsInPlane()
+
+    for point in test_input:
+        x_value = point[0]
+        y_value = point[1]
+
+        context.assertEqual(line_equation_1(x_value, y_value), line_equation_2(x_value, y_value))
+
+
+def assertListsEqual(context, list1, list2):
+    context.assertTrue(len(list1) == len(list2) and sorted(list1) == sorted(list2))
